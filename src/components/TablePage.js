6ebed1465d575f29cbe0d2fdbf454ed48faf61d2
karataev <eugene.karataev@gmail.com>
2018-05-16 13:30:58 +0700

    Add extra long format

--- src/components/TablePage.js
+++ src/components/TablePage.js
@@ -19,6 +19,18 @@ const longTable = [
 ]
   .map(row => row.split(/\s+/));
 
+const extraLongTable = [
+  'x   g1  x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   g18',
+  'p1  H   g2  x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   g13 g14 g15 g16 g17 He',
+  'p2  Li  Be  x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   B   C   N   O   F   Ne',
+  'p3  Na  Mg  x   x   x   x   x   x   x   x   x   x   x   x   x   x   g3  g4  g5  g6  g7  g8  g9  g10 g11 g12 Al  Si  P   S   Cl  Ar',
+  'p4  K   Ca  x   x   x   x   x   x   x   x   x   x   x   x   x   x   Sc  Ti  V   Cr  Mn  Fe  Co  Ni  Cu  Zn  Ga  Ge  As  Se  Br  Kr',
+  'p5  Rb  Sr  x   x   x   x   x   x   x   x   x   x   x   x   x   x   Y   Zr  Nb  Mo  Tc  Ru  Rh  Pd  Ag  Cd  In  Sn  Sb  Te  I   Xe',
+  'p6  Cs  Ba  La  Ce  Pr  Nd  Pm  Sm  Eu  Gd  Tb  Dy  Ho  Er  Tm  Yb  Lu  Hf  Ta  W   Re  Os  Ir  Pt  Au  Hg  Tl  Pb  Bi  Po  At  Rn',
+  'p7  Fr  Ra  Ac  Th  Pa  U   Np  Pu  Am  Cm  Bk  Cf  Es  Fm  Md  No  Lr  Rf  Db  Sg  Bh  Hs  Mt  Ds  Rg  Cn  Nh  Fl  Mc  Lv  Ts  Og',
+]
+  .map(row => row.split(/\s+/));
+
 
 export default class TablePage extends React.Component {
 
@@ -59,7 +71,7 @@ export default class TablePage extends React.Component {
       <Fragment>
         <CategoryList/>
         <ElementTable
-          data={longTable}
+          data={extraLongTable}
           onElementSelect={this.onElementSelect}
         />
         <Dialog
