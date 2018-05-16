7377ee0c6012c97ca99eaaf0d267f13a78e248b2
karataev <eugene.karataev@gmail.com>
2018-05-16 12:34:10 +0700

    Show modal on element's click with wiki link and element's details

--- src/components/TablePage.js
+++ src/components/TablePage.js
@@ -1,4 +1,6 @@
 import React, {Fragment} from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 import ElementTable from "../table/ElementTable";
 import CategoryList from "../categories/CategoryList";
@@ -6,17 +8,64 @@ import CategoryList from "../categories/CategoryList";
 
 export default class TablePage extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedElement: null,
+    }
+  }
+
   onElementSelect = el => {
-    console.log('select', el);
+    this.setState({selectedElement: el});
   };
 
+  handleClose = () => {
+    this.setState({selectedElement: null});
+  };
+
+  getWikiLink(name) {
+    return `https://ru.wikipedia.org/wiki/${name}`;
+  }
+
   render() {
+    const actions = [
+      <FlatButton
+        label="Ok"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+    ];
+
+    const {selectedElement} = this.state;
+    const title = selectedElement && selectedElement.name_ru || '';
+    const isOpen = !!this.state.selectedElement;
+
     return (
       <Fragment>
         <CategoryList/>
         <ElementTable
           onElementSelect={this.onElementSelect}
         />
+        <Dialog
+          title={title}
+          actions={actions}
+          modal={false}
+          open={isOpen}
+          onRequestClose={this.handleClose}
+        >
+          {selectedElement && (
+            <div>
+              <p>
+                <a href={this.getWikiLink(selectedElement.name_ru)} target="_blank">Википедия</a>
+              </p>
+              <p>
+                Электронные оболочки: {selectedElement.shells.join(', ')}
+              </p>
+            </div>
+          )}
+
+        </Dialog>
       </Fragment>
     )
   }
