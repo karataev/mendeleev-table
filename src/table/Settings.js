be25eb93e8a5a71ec39d21f9adf59d9ec93b14db
karataev <eugene.karataev@gmail.com>
2018-05-17 21:15:18 +0700

    Make settings expanding great again

--- src/table/Settings.js
+++ src/table/Settings.js
@@ -1,18 +1,11 @@
-import React, {Fragment} from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import {Subscribe} from 'unstated';
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 
 import AppStore from '../stores/AppStore';
 import CategoryList from "../categories/CategoryList";
-
-const Root = styled.div`
-margin: 10px;
-padding: 10px;
-box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 6px;
-`;
 
 export default class Settings extends React.Component {
 
@@ -24,10 +17,10 @@ export default class Settings extends React.Component {
           actAsExpander={true}
           showExpandableButton={true}
         />
-        <Subscribe to={[AppStore]}>
-          {store => (
-            <Fragment>
-              <CardText expandable={true}>
+        <CardText expandable={true}>
+          <Subscribe to={[AppStore]}>
+            {store => (
+              <div>
                 <SelectField
                   floatingLabelText="Форма таблицы"
                   value={store.state.tableForm.id}
@@ -41,8 +34,6 @@ export default class Settings extends React.Component {
                     />
                   ))}
                 </SelectField>
-              </CardText>
-              <CardText expandable={true}>
                 <SelectField
                   value={store.getColorGroup().id}
                   onChange={(event, index, value) => store.setColorGroup(value)}
@@ -57,10 +48,10 @@ export default class Settings extends React.Component {
                   ))}
                 </SelectField>
                 <CategoryList items={store.getColorGroup().data}/>
-              </CardText>
-            </Fragment>
-          )}
-        </Subscribe>
+              </div>
+            )}
+          </Subscribe>
+        </CardText>
       </Card>
     )
   }
